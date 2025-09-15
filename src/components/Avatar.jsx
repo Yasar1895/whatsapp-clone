dcc79558a33c10221edc9e3689b16c824e082685
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 11:00:44 +0530

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function Avatar({ src, alt, size = 44 }) {
+function Avatar({ src, size = 45 }) {
   return (
-    <div className="avatar" style={{ width: size, height: size }}>
-      <img src={src} alt={alt || 'avatar'} />
-    </div>
-  )
+    <img
+      src={src}
+      alt="avatar"
+      className="avatar"
+      style={{
+        width: size,
+        height: size,
+        borderRadius: "50%",
+        objectFit: "cover"
+      }}
+    />
+  );
 }
+
+export default Avatar;
