e4a4371005ad0dbb463b85c27f7911857a476eae
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 10:28:43 +0530

    Create users.js

---
+++ src/data/users.js
@@ -0,0 +1,29 @@
+// sample users (you can change avatars to your own)
+const users = [
+  {
+    id: 'u1',
+    name: 'You',
+    phone: '+91 98765 43210',
+    avatar: 'https://i.pravatar.cc/150?img=12' // current user
+  },
+  {
+    id: 'u2',
+    name: 'Aisha Khan',
+    avatar: 'https://i.pravatar.cc/150?img=5',
+    about: 'Available'
+  },
+  {
+    id: 'u3',
+    name: 'Rahul Verma',
+    avatar: 'https://i.pravatar.cc/150?img=6',
+    about: 'At work'
+  },
+  {
+    id: 'u4',
+    name: 'Family Group',
+    avatar: 'https://i.pravatar.cc/150?img=15',
+    about: '3 members'
+  }
+]
+
+export default users
