8689e57fd977e209089e0c4fa7d448ef5fb04ea3
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 10:55:17 +0530

    Update users.js

--- src/data/users.js
+++ src/data/users.js
@@ -1,18 +1,33 @@
 const users = [
   {
     id: 1,
-    name: "Alice",
-    avatar: "https://i.pravatar.cc/150?img=1"
+    name: "John Cena",
+    avatar: "https://i.pravatar.cc/150?img=11"
   },
   {
     id: 2,
-    name: "Bob",
-    avatar: "https://i.pravatar.cc/150?img=2"
+    name: "The Undertaker",
+    avatar: "https://i.pravatar.cc/150?img=12"
   },
   {
     id: 3,
-    name: "Charlie",
-    avatar: "https://i.pravatar.cc/150?img=3"
+    name: "The Rock",
+    avatar: "https://i.pravatar.cc/150?img=13"
+  },
+  {
+    id: 4,
+    name: "Stone Cold",
+    avatar: "https://i.pravatar.cc/150?img=14"
+  },
+  {
+    id: 5,
+    name: "Triple H",
+    avatar: "https://i.pravatar.cc/150?img=15"
+  },
+  {
+    id: 6,
+    name: "Shawn Michaels",
+    avatar: "https://i.pravatar.cc/150?img=16"
   }
 ];
 
