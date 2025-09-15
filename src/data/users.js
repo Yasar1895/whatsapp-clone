a5dadc15e2cbb0942c90c5fee8616570dd980727
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 10:59:45 +0530

    Update users.js

--- src/data/users.js
+++ src/data/users.js
@@ -2,32 +2,32 @@ const users = [
   {
     id: 1,
     name: "John Cena",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/6/6d/John_Cena_2012.jpg"
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/John_Cena_2012.jpg/200px-John_Cena_2012.jpg"
   },
   {
     id: 2,
     name: "The Undertaker",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/0/02/The_Undertaker_WM31.jpg"
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/The_Undertaker_WM31.jpg/200px-The_Undertaker_WM31.jpg"
   },
   {
     id: 3,
     name: "The Rock",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/f/f6/Dwayne_Johnson_2%2C_2013.jpg"
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Dwayne_Johnson_2%2C_2013.jpg/200px-Dwayne_Johnson_2%2C_2013.jpg"
   },
   {
     id: 4,
     name: "Stone Cold",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/7/77/Stone_Cold_Steve_Austin_2010.jpg"
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Stone_Cold_Steve_Austin_2010.jpg/200px-Stone_Cold_Steve_Austin_2010.jpg"
   },
   {
     id: 5,
     name: "Triple H",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/2/2b/Triple_H_2017.jpg"
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Triple_H_2017.jpg/200px-Triple_H_2017.jpg"
   },
   {
     id: 6,
     name: "Shawn Michaels",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Shawn_Michaels_WM31_Axxess.jpg"
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Shawn_Michaels_WM31_Axxess.jpg/200px-Shawn_Michaels_WM31_Axxess.jpg"
   }
 ];
 
